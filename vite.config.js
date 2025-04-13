55c2f6942880f907c609f268d55f67b7a95c7507
vannystark3 <adityakhurana.3760@gmail.com>
2025-04-13 21:19:21 +0530

    kuchh to kiya hai

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', // Make sure the build folder is correctly specified
     rollupOptions: {
-      external: ['react-slick', 'slick-carousel/slick/slick.css', 'slick-carousel/slick/slick-theme.css'],
+      external: [
+        'react-slick',
+        'slick-carousel/slick/slick.css',
+        'slick-carousel/slick/slick-theme.css',
+      ],
     },
   },
 });
