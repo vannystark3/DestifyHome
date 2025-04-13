804024ffdf8cedbeef16e9bf269d5b4e1a8bc05e
vannystark3 <adityakhurana.3760@gmail.com>
2025-04-13 21:32:08 +0530

    akhri baar

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
-    outDir: 'dist', // Make sure the build folder is correctly specified
-    rollupOptions: {
-      external: [
-        'react-slick',
-        'slick-carousel/slick/slick.css',
-        'slick-carousel/slick/slick-theme.css',
-      ],
-    },
+    outDir: 'dist',
   },
 });
